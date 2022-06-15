c2709f7a42788196a468c96b27bf7fe9705cc6fc
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2022-06-15 20:46:45 +0300

    Initial Commit

---
+++ ContacnsSwiftUI/ContacnsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContacnsSwiftUIApp.swift
+//  ContacnsSwiftUI
+//
+//  Created by Виталий Подшибякин on 15.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContacnsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
